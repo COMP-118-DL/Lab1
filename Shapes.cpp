69820ee909e862c042d3cf6cb50baa2b447bff7e
CoolMaD <pglevinson@outlook.com>
2024-03-04 09:32:16 +0200

    Added Menu

---
+++ Shapes.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Daniel on 04/03/2024.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int userChoice;
+
+    cout << "1) Draw a horizontal line" << endl;
+    cout << "2) Draw a vertical line" << endl;
+    cout << "3) Draw a square" << endl;
+    cout << "4) Draw a rectangle" << endl;
+    cout << "5) Quit" << endl;
+
+    cout << "Enter your option: ";
+    cin >> userChoice;
+
+    return 0;
+}
