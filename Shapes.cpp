a74ddb7ba0fe8447e0fb3c4dc9b081fcdf470c82
CoolMaD <pglevinson@outlook.com>
2024-03-04 09:50:00 +0200

    Added drawHorizontalLine

--- Shapes.cpp
+++ Shapes.cpp
@@ -14,6 +14,8 @@ void drawRectangle(int height, int length, char ch);
 int main() {
 
     int userChoice;
+    int size;
+    char ch;
 
     cout << "1) Draw a horizontal line" << endl;
     cout << "2) Draw a vertical line" << endl;
@@ -24,11 +26,25 @@ int main() {
     cout << "Enter your option: ";
     cin >> userChoice;
 
+    switch (userChoice) {
+        case 1:
+            cout << "\nInput the length and character of the line:";
+            cin >> size >> ch;
+            drawHorizontalLine(size, ch);
+            break;
+    }
+
     return 0;
 }
 
 void drawHorizontalLine(int length, char ch) {
-    cout << "\nI am drawHorizontalLine" << endl;
+    cout << "Horizontal line (" << length << "," << ch << ")" << endl;
+
+    for (int i = 0; i < length; i++) {
+        cout << ch;
+    }
+    cout << endl;
+
 }
 
 void drawVerticalLine(int height, char ch) {
